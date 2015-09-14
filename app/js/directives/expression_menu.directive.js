32b99989d8c9fdb5bf7984bdb4ec1437c7952d0c
Adam Robbie <adam.robbie@gmail.com>
2015-09-14 10:07:17 -0400

    start expression menu

---
+++ app/js/directives/expression_menu.directive.js
@@ -0,0 +1,9 @@
+angular.module('app')
+  .directive('expressionMenu', [function() {
+    return {
+      restrict: 'A',
+      link: function(scope, elem, attrs) {
+        $('div.dropdown').dropdown();
+      }
+    };
+}])
